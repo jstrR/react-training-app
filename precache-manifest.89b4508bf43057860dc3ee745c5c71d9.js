c1a0646581927af9bc99127eb64aa8db5538eae9
Siarhei Roukach <srovk22@gmail.com>
2019-03-06 18:49:39 +0300

    Updates

---
+++ precache-manifest.89b4508bf43057860dc3ee745c5c71d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eea7f8041fa2a7581c35",
+    "url": "/react-training-app/static/css/main.c2edddab.chunk.css"
+  },
+  {
+    "revision": "eea7f8041fa2a7581c35",
+    "url": "/react-training-app/static/js/main.eea7f804.chunk.js"
+  },
+  {
+    "revision": "277b539c14d939b4133b",
+    "url": "/react-training-app/static/js/runtime~main.277b539c.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "/react-training-app/static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "c679f63428ad99514e5f4b009e245079",
+    "url": "/react-training-app/index.html"
+  }
+];
